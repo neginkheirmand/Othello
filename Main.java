e52a0fc87b2113fdfdff4709d6ce60ed691fe4af
9831023 <9831023@ceit.local>
2020-04-08 09:51:06 +0430

    the final version of the Main.java file

--- Main.java
+++ Main.java
@@ -5,6 +5,9 @@
  import java.util.ArrayList;
  import java.util.Scanner;
 
+ /**
+  * an enum which will help to transform the number to the Capital letter in the top of the board
+  */
  enum X{
      A,
      B,
@@ -14,6 +17,12 @@
      F,
      G,
      H;
+
+     /**
+      * a method which will return a String holding the specifiend char for the input integer
+      * @param xNum
+      * @return
+      */
      public static String valueOfInt(int xNum){
        for(int i=0; i<X.values().length; i++){
            if(X.values()[i].ordinal()==xNum){
@@ -25,165 +34,13 @@
  }
 
 
+ /**
+  * This is the Main class
+  * @author negin
+  * @version 1.0
+  */
  public class Main {
     public static void main(String args[]){
         new Run();
     }
  }
-
- /*
-
- Scanner wait=new Scanner(System.in);
-//
-//        playerBlack = new Player(TYPE.BLACK);
-//        playerWhite = new Player(TYPE.WHITE);
-//        String nextMoveBlack;
-//        String nextMoveWhite;
-//
-//        int newX=0, newY=0;
-//        while(myOthelloBoard.gameEnd()==false){
-//            //first move to black pawn
-//            if(playerBlack.nextMoveUpdate(myOthelloBoard)!=0) {
-//                System.out.printf("TURN OF BLACK: \n");
-//                myOthelloBoard.printBoard(playerBlack.getMovesAvailable(), playerBlack.getTypeOfPlayer());
-////                boolean validInput = false;
-////                int numOfWrongInputs = 0;
-////                do {
-////                    if (numOfWrongInputs > 0) {
-////                        System.out.printf("you have to enter a valid direction of block, pay attention to the blocks filled with:");
-////                        System.out.printf("\033[0;31m" + "x\n" + "\033[0m");
-////                        validInput = false;
-////                    }
-////
-////                    do {
-////                        nextMoveBlack = wait.nextLine();
-////                        try {
-////                            newY = Integer.parseInt(nextMoveBlack.charAt(0) + "");
-////                            newY--;
-////                            newX = X.valueOf(nextMoveBlack.charAt(1) + "").ordinal();
-////                            System.out.println("x is = " + newX + "  y = " + newY);
-////                            validInput = true;
-////                        } catch (IllegalArgumentException e) {
-////                            validInput = false;
-////                            System.out.println("the format should be IC with no space in between and the I should be a valid number");
-////                            System.out.println("the C should be a valid letter with Capital mode--> \"3C\"\n");
-////                        }
-////                    } while (!validInput);
-////                    numOfWrongInputs++;
-////                } while (!playerBlack.canAddDisk(newX, newY, myOthelloBoard));
-//                playerBlack.addDisk(newX, newY, myOthelloBoard);
-//            }
-//            else{
-//                System.out.println("Black cannot play\nPASS");
-//            }
-//            //------------------------------
-//            if(playerWhite.nextMoveUpdate(myOthelloBoard)!=0) {
-//                System.out.println("TURN OF WHITE:");
-//                myOthelloBoard.printBoard(playerWhite.getMovesAvailable(), playerWhite.getTypeOfPlayer());
-//                boolean validInput = false;
-//                int numOfWrongInputs = 0;
-//                do {
-//                    if (numOfWrongInputs > 0) {
-//                        System.out.printf("you have to enter a valid direction of block, pay atention to the blocks filled with:");
-//                        System.out.printf("\033[1;37m" + "x\n" + "\033[0m");
-//                        validInput = false;
-//                    }
-//
-//                    do {
-//                        nextMoveWhite = wait.nextLine();
-//                        try {
-//                            newY = Integer.parseInt(nextMoveWhite.charAt(0) + "");
-//                            newY--;
-//                            newX = X.valueOf(nextMoveWhite.charAt(1) + "").ordinal();
-//                            validInput = true;
-//                        } catch (IllegalArgumentException e) {
-//                            validInput = false;
-//                            System.out.println("the format should be IC with no space in between and the I should be a valid number");
-//                            System.out.println("the C should be a valid letter with Capital mode--> \"3C\"\n");
-//                        }
-//                    } while (!validInput);
-//                    numOfWrongInputs++;
-//                } while (!playerWhite.canAddDisk(newX, newY, myOthelloBoard));
-//                playerWhite.addDisk(newX, newY, myOthelloBoard);
-//            }
-//            else{
-//                System.out.println("White cannot play\nPASS");
-//            }
-
-  */
-
-
-
-
-
-/*
-
-
-    public static void main(String[] args) {
-
-        if(playerType==1){
-
-        }else{
-
-        }
-        PcPlayer playerBlack;
-        Player playerWhite;
-        //inja farz shode ke black ai hast
-        playerBlack= new PcPlayer(TYPE.BLACK);
-        playerWhite= new Player(TYPE.WHITE);
-        Board myOthelloBoard=new Board();
-
-        //---------------
-        String nextMoveWhite;
-        while(myOthelloBoard.gameEnd()==false){
-            //first move to black pawn
-            System.out.printf("TURN OF BLACK: \n");
-            playerBlack.nextMoveUpdate(myOthelloBoard);
-            myOthelloBoard.printBoard(playerBlack.getMovesAvailable(), playerBlack.getTypeOfPlayer());
-            playerBlack.decideNextMove(myOthelloBoard);
-            myOthelloBoard.printStableDisks(TYPE.BLACK);
-
-            int newX=0, newY=0;
-            if(playerWhite.nextMoveUpdate(myOthelloBoard)!=0) {
-                System.out.println("TURN OF WHITE:");
-                myOthelloBoard.printBoard(playerWhite.getMovesAvailable(), playerWhite.getTypeOfPlayer());
-                boolean validInput = false;
-                int numOfWrongInputs = 0;
-                do {
-                    if (numOfWrongInputs > 0) {
-                        System.out.printf("you have to enter a valid direction of block, pay atention to the blocks filled with:");
-                        System.out.printf("\033[1;37m" + "x\n" + "\033[0m");
-                        validInput = false;
-                    }
-
-                    do {
-                        nextMoveWhite = wait.nextLine();
-                        try {
-                            newY = Integer.parseInt(nextMoveWhite.charAt(0) + "");
-                            newY--;
-                            newX = X.valueOf(nextMoveWhite.charAt(1) + "").ordinal();
-                            validInput = true;
-                        } catch (IllegalArgumentException e) {
-                            validInput = false;
-                            System.out.println("the format should be IC with no space in between and the I should be a valid number");
-                            System.out.println("the C should be a valid letter with Capital mode--> \"3C\"\n");
-                        }
-                    } while (!validInput);
-                    numOfWrongInputs++;
-                } while (!playerWhite.canAddDisk(newX, newY, myOthelloBoard));
-                playerWhite.addDisk(newX, newY, myOthelloBoard);
-            }
-            else{
-                System.out.println("White cannot play\nPASS");
-            }
-        }
-
-
-
-
-        System.out.printf("----------------\nend of game--------------\n");
-        myOthelloBoard.printBoard( new ArrayList<Place>(), playerBlack.getTypeOfPlayer());
-        myOthelloBoard.printInfo();
-        //---------------
-
-* */
