bcad312b957fe638b2ae5e219ce84ec8fb4c4a60
9831023 <9831023@ceit.local>
2020-04-08 09:53:09 +0430

    the final version of the Player.java file

--- Player.java
+++ Player.java
@@ -5,39 +5,64 @@ import ir.ac.aut.TYPE;
 
 import java.util.ArrayList;
 
+
+/**
+ * This is a model class to hold player information
+ *
+ * @author negin
+ * @version 1.0
+ */
+
 public class Player {
 
+    //a protected field for rep
     protected final TYPE typeOfPlayer;
-    protected ArrayList<Place> movesAvailable;
-    protected long timer;
 
+    //an array list to hold the places available for this player
+    protected ArrayList<Place> movesAvailable;
+
+    /**
+     * a constructor
+     * @param typeOfPlayer type of player
+     */
     public Player(TYPE typeOfPlayer){
         this.typeOfPlayer=typeOfPlayer;
-        timer=0;
     }
 
+    /**
+     * a getter method for the movesAvailable field
+     * @return movesAvailable
+     */
     public ArrayList<Place> getMovesAvailable(){
         return movesAvailable;
     }
 
+    /**
+     * a getter method for the typeOfPlayer field
+     * @return typeOfPlayer
+     */
     public TYPE getTypeOfPlayer(){
         return typeOfPlayer;
     }
 
+    /**
+     * a method for updating the movesAvailable field and returning its size
+     * @param gameBoard the game board
+     * @return the size of movesAvailable field after being updated
+     */
     public int nextMoveUpdate(Board gameBoard){
 //        System.out.println("updating the next moves for the player: "+this.typeOfPlayer);
         movesAvailable=gameBoard.updateAvailableMoves(typeOfPlayer);
         return movesAvailable.size();
     }
-    public void printInfo(){
-        System.out.println("TYPE of this player :"+typeOfPlayer.name()+"\nand has"+ movesAvailable.size()+"moves available to do");
 
-        for(int i=0;i<movesAvailable.size();i++){
-            movesAvailable.get(i).printInfo();
-        }
-        System.out.println("\n\n");
-    }
-
+    /**
+     * a method for checking if a disk with TYPE of this players TYPE can be added to an specified location in the board
+     * @param x x of the location
+     * @param y y of the location
+     * @param othelloBoard
+     * @return a boolean to specify if a disk can be added to the block
+     */
     public boolean canAddDisk(int x, int y, Board othelloBoard){
         boolean canBePlaced=false;
         for(int i=0; i<movesAvailable.size(); i++){
@@ -48,7 +73,12 @@ public class Player {
         return false;
     }
 
-
+    /**
+     * a method for adding a disk in the specified block
+     * @param x x of the block
+     * @param y y of the block
+     * @param othelloBoard the game board
+     */
     public void addDisk(int x, int y, Board othelloBoard){
         othelloBoard.addDiskToBoardAndUpdate(x, y, this.typeOfPlayer);
         return;
